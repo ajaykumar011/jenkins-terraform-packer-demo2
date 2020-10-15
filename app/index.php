b2d038f85dbd2fd4df78746dc5ffd0a29b5a6ed2
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-15 13:00:36 +0000

    update

---
+++ app/index.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+<html>
+<head>
+<style>
+body { 
+    background-image: url('coming_soon.jpeg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center; 
+}
+</style>
+</head>
+<body>
+</body>
+</html> 
+
+<?php
+
+$domain = $_SERVER['SERVER_NAME'];
+echo "<BR><div style='text-align:center'><h1>".$domain."</h1></div>";
+
+
+?>
+
