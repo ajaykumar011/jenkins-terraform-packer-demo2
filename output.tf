956dd3e3a7765feadaadd11b014195124e403bef
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-16 11:47:02 +0000

    update

--- output.tf
+++ output.tf
@@ -1,4 +1,14 @@
-output "ELB" {
-  value = aws_elb.app-alb.dns_name
+output "this_lb_id" {
+  description = "The ID and ARN of the load balancer we created."
+  value       = module.alb.this_lb_id
 }
 
+output "this_lb_arn" {
+  description = "The ID and ARN of the load balancer we created."
+  value       = module.alb.this_lb_arn
+}
+
+output "this_lb_dns_name" {
+  description = "The DNS name of the load balancer."
+  value       = module.alb.this_lb_dns_name
+}
