06f89f8f7a2e6bb609ff5a7d16883c5202ac4d39
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-16 18:48:45 +0000

    update

--- output.tf
+++ output.tf
@@ -1,14 +1,3 @@
-output "this_lb_id" {
-  description = "The ID and ARN of the load balancer we created."
-  value       = module.alb.this_lb_id
-}
-
-output "this_lb_arn" {
-  description = "The ID and ARN of the load balancer we created."
-  value       = module.alb.this_lb_arn
-}
-
-output "this_lb_dns_name" {
-  description = "The DNS name of the load balancer."
-  value       = module.alb.this_lb_dns_name
+output "ELB" {
+  value = aws_lb.app-alb.dns_name
 }
