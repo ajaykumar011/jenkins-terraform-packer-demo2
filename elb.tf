314394f43907423be801dd54d31aef3977333742
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-16 04:04:08 +0000

    update

--- elb.tf
+++ elb.tf
@@ -2,25 +2,58 @@ resource "aws_elb" "app-elb" {
   name            = "app-elb"
   subnets         = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
   security_groups = [aws_security_group.elb-securitygroup.id]
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
+}
+
+resource "aws_elb_listener" "app-elb" {
+  load_balancer_arn = aws_elb.app-elb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_elb_target_group.app-elb-blue.id
+    target_group_index = 0
+    type             = "forward"
   }
+  lifecycle {
+    ignore_changes = [
+      default_action,
+    ]
+  }
+}
+
+resource "aws_elb_target_group" "app-elb-blue" {
+  name                 = "app-http-blue"
+  port                 = "80"
+  protocol             = "HTTP"
+  target_type          = "instance"
+  vpc_id               = aws_vpc.main.id
+  deregistration_delay = "30"
+
   health_check {
-    healthy_threshold   = 2
+    healthy_threshold   = 5
     unhealthy_threshold = 2
-    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+    timeout             = 5
+  }
+}
+resource "aws_elb_target_group" "app-elb-green" {
+  name                 = "app-http-green"
+  port                 = "80"
+  protocol             = "HTTP"
+  target_type          = "instance"
+  vpc_id               = aws_vpc.main.id
+  deregistration_delay = "30"
+
+  health_check {
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 30
     target              = "HTTP:80/"
     interval            = 30
   }
-
-  cross_zone_load_balancing   = true  #distribute traffic across zones
-  connection_draining         = true
-  connection_draining_timeout = 400    #seconds to drain all active connections before termination of instance
-  tags = {
-    Name = "app-elb"
-  }
 }
+
+
+
 
