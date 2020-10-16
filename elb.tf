3a2e033d409a9997ba34520376facb8cf85f0294
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-16 07:29:44 +0000

    udate

--- elb.tf
+++ elb.tf
@@ -1,61 +1,46 @@
-resource "aws_elb" "app-elb" {
-  name            = "app-elb"
-  subnets         = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
-  security_groups = [aws_security_group.elb-securitygroup.id]
-  tags {
-    Name = "app-elb-tag"
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 5.0"
+  
+  name = "app-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = aws_vpc.main.id
+  subnets            = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
+  security_groups    = [aws_security_group.elb-securitygroup.id]
+  
+  access_logs = {
+    bucket = "cloudzone100"
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  // https_listeners = [
+  //   {
+  //     port               = 443
+  //     protocol           = "HTTPS"
+  //     certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+  //     target_group_index = 0
+  //   }
+  // ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Test"
   }
 }
-
-resource "aws_elb_listener" "app-elb-http" {
-  load_balancer_arn = aws_elb.app-elb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = aws_elb_target_group.app-elb-tg1.id
-    // target_group_index = 0
-    type             = "forward"
-  }
-  lifecycle {
-    ignore_changes = [
-      default_action,
-    ]
-  }
-}
-
-// resource "aws_elb_listener" "app-elb-https" {
-//   load_balancer_arn = aws_elb.app-elb.arn
-//   port              = "443"
-//   protocol          = "HTTPS"
-//   ssl_policy        = "ELBSecurityPolicy-2016-08"
-//   certificate_arn   = var.certificate_arn
-
-//   default_action {
-//     target_group_arn = aws_elb_target_group.app-elb-tg2.id
-//     // target_group_index = 0
-//     type             = "forward"
-//   }
-// }
-
-resource "aws_elb_target_group" "app-elb-tg1" {
-  name                 = "app-elb-tg1"
-  port                 = "80"
-  protocol             = "HTTP"
-  target_type          = "instance"
-  vpc_id               = aws_vpc.main.id
-  deregistration_delay = "30"
-
-  health_check {
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    target              = "HTTP:80/"
-    interval            = 30
-    timeout             = 5
-  }
-}
-
-
-
-
-
