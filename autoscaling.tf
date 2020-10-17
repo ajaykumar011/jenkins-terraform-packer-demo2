b1092c888ac48498fdcbb466dd63e31967b95fd2
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-17 10:04:47 +0000

    update

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,13 +1,13 @@
 resource "aws_launch_template" "app-launchtp" {
   name = "app-launchtp"
 
-  block_device_mappings {
-    device_name = "/dev/sda1"
+  // block_device_mappings {
+  //   device_name = "/dev/sda1"
 
-    ebs {
-      volume_size = 20
-    }
-  }
+  //   ebs {
+  //     volume_size = 20
+  //   }
+  // }
 
   // cpu_options {
   //   core_count       = 1
@@ -18,21 +18,21 @@ resource "aws_launch_template" "app-launchtp" {
   //   cpu_credits = "standard"
   // }
 
-  disable_api_termination = true
-  ebs_optimized = true
+  // disable_api_termination = true
+  // ebs_optimized = true
 
   iam_instance_profile {
     name = aws_iam_instance_profile.app-ec2-role.name
   }
 
   image_id = var.APP_AMI
-  instance_initiated_shutdown_behavior = "terminate"
+  //instance_initiated_shutdown_behavior = "terminate"
   instance_type = "t2.micro"
   key_name = aws_key_pair.mykey.key_name
 
-  monitoring {
-    enabled = true
-  }
+  // monitoring {
+  //   enabled = true
+  // }
 
   // network_interfaces {
   //   associate_public_ip_address = true
@@ -56,11 +56,11 @@ resource "aws_launch_template" "app-launchtp" {
 resource "aws_autoscaling_group" "app-launchtp-asg" {
   name                      = "app-launchtp-asg"
   vpc_zone_identifier       = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
-  desired_capacity          = 2
-  min_size                  = 2
-  max_size                  = 2
-  health_check_grace_period = 300
-  health_check_type         = "EC2"  #this is important 
+  desired_capacity          = 1
+  min_size                  = 1
+  max_size                  = 1
+  //health_check_grace_period = 300
+  //health_check_type         = "EC2"  #this is important 
   target_group_arns         = [aws_lb_target_group.app-alb-tg1.arn]
   force_delete              = true
   launch_template {
