e0120a395de20f7d8284bb853bb2d7ff06454726
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-17 11:27:00 +0000

    udpate

--- autoscaling.tf
+++ autoscaling.tf
@@ -28,6 +28,7 @@ resource "aws_launch_template" "app-launchtp" {
   network_interfaces {
     associate_public_ip_address = true
     delete_on_termination = true
+    security_groups = [aws_security_group.myinstance.id]
    }
   // placement {
   //   availability_zone = "us-east-1a"
